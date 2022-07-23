85e67dda1863c1c6acc0e45a332a6f6e80f5daf7
srivarkor <srivardhanakorada@digiframe.org>
2022-07-23 21:18:40 +0530

    Header Component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+
+export class Header extends React.Component{
+    render(){
+        return(
+            <div id="header">
+                <h1>My Weather</h1>
+            </div>
+
+        )
+    }
+}
